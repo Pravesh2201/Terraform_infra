20233f1965c1ab07ad51622631d4caf4b13e7c3f
Pravesh Kumar <161683443+Pravesh2201@users.noreply.github.com>
2024-10-01 13:03:45 +0530

    Update variables.tf

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -1,6 +1,19 @@
-variable "vpc_id" {}
-variable "public_subnet_id" {}
-variable "private_subnet_id" {}
-variable "bastion_sg_id" {}
-variable "private_sg_id" {}
-variable "key_name" {}
+variable "vpc_id" {
+    type = string
+}
+variable "public_subnet_id" {
+    type = string
+}
+variable "private_subnet_id" {
+    type = string
+}
+variable "bastion_sg_id" {
+    type = string
+}
+variable "private_sg_id" {
+    type = string
+}
+variable "key_name" {
+    type = string
+}
+
