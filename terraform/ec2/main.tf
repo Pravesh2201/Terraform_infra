108f2bd11e8014e6720180a7975f343930578e58
Pravesh Kumar <161683443+Pravesh2201@users.noreply.github.com>
2024-10-01 13:03:29 +0530

    Update main.tf

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,12 +1,11 @@
+
 # Bastion EC2 Instance
 resource "aws_instance" "bastion" {
   ami           = "ami-0a0e5d9c7acc336f1"
   instance_type = "t2.micro"
   key_name      = var.key_name
   subnet_id     = var.public_subnet_id
-  security_groups = var.bastion_sg_id
-#   security_group_ids = var.bastion_sg_id
-
+  security_groups = [var.bastion_sg_id]
 
   tags = {
     Name = "Bastion Host"
@@ -19,8 +18,7 @@ resource "aws_instance" "private_ec2" {
   instance_type = "t2.micro"
   key_name      = var.key_name
   subnet_id     = var.private_subnet_id
-  security_groups = var.private_sg_id
-  
+  security_groups = [var.private_sg_id] # Use security_group_ids here
 
   tags = {
     Name = "Private EC2 (Tool)"
