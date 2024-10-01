864e23d67c619ca297a3b1b16a6004e97c28cc30
Pravesh Kumar <161683443+Pravesh2201@users.noreply.github.com>
2024-10-01 16:00:40 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,13 @@ output "private_ec2_private_ip" {
 output "vpc_peering_connection_id" {
   value = module.vpc_peering.vpc_peering_connection_id
 }
+
+output "IP_Public_Bastion" {
+  value = module.ec2.bastion_public_ip
+  
+}
+
+output "IP_elasticsearch" {
+  value = module.ec2.private_ec2_private_ip
+  
+}
